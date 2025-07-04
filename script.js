bb651b6f8c99fd6a41a91a2472f88f77f9b4f03e
Amar Pratap Singh <165669341+amarpratapsingh2452@users.noreply.github.com>
2025-07-04 11:58:06 +0530

    added score functionality

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let userChoice = ""; 
+let userscore = 0;
+let compscore = 0;
+
 document.getElementsByClassName('scissor')[0].addEventListener("click", () => {
     userChoice = "scissor";
     document.getElementsByClassName('box2')[0].innerHTML = `
@@ -25,10 +28,8 @@ document.getElementsByClassName('paper')[0].addEventListener("click", () => {
 
 const winning_arr = [["stone", "scissor"], ["paper", "stone"], ["scissor", "paper"]];
 const draw_arr = [["stone", "stone"], ["scissor", "scissor"], ["paper", "paper"]];
-const lose_arr = [["stone", "paper"], ["paper", "scissor"], ["scissor", "stone"]];
 
-
-document.getElementsByClassName('go')[0].addEventListener("click", () => {
+function playRound() {
     if (userChoice === "") {
         alert("Please choose Stone, Paper, or Scissor first!");
         return;
@@ -37,7 +38,6 @@ document.getElementsByClassName('go')[0].addEventListener("click", () => {
     let num = Math.floor(Math.random() * 3);
     let compChoice = ["stone", "paper", "scissor"][num];
 
-    
     let compHTML = "";
     if (compChoice === "stone") {
         compHTML = `<img src="https://www.nerdswithknives.com/wp-content/uploads/2018/03/Fieldstone-1024x588.png"><span>stone</span>`;
@@ -49,22 +49,41 @@ document.getElementsByClassName('go')[0].addEventListener("click", () => {
 
     document.getElementsByClassName('box4')[0].innerHTML = compHTML;
 
-    
     let result = "";
     if (winning_arr.some(pair => pair[0] === userChoice && pair[1] === compChoice)) {
         result = "YOU WIN! 🎉";
+        userscore++;
     } else if (draw_arr.some(pair => pair[0] === userChoice && pair[1] === compChoice)) {
         result = "DRAW 😐";
     } else {
         result = "YOU LOSE! 😢";
+        compscore++;
     }
 
-    
+ 
     document.getElementsByClassName('box3')[0].innerHTML = `
-        <button class="go">GO</button>
         <div class="result">${result}</div>
+        <button class="play-again">Play Again</button>
     `;
-});
+
+
+    document.querySelector(".user").innerText = `You: ${userscore}`;
+    document.querySelector(".computer").innerText = `Computer: ${compscore}`;
+
+ 
+    document.querySelector('.play-again').addEventListener("click", () => {
+        userChoice = "";
+        document.getElementsByClassName('box2')[0].innerHTML = "";
+        document.getElementsByClassName('box4')[0].innerHTML = "";
+        document.getElementsByClassName('box3')[0].innerHTML = `
+            <button class="go">GO</button>
+        `;
+        document.getElementsByClassName('go')[0].addEventListener("click", playRound);
+    });
+}
+
+
+document.getElementsByClassName('go')[0].addEventListener("click", playRound);
 
 
 document.querySelector('.refresh').addEventListener("click", () => {
